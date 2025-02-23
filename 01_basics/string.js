0b75b96f17124623b751109069ecdf308bc20236
Aditya-43 <69339559+Aditya-43@users.noreply.github.com>
2025-02-23 07:57:18 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+const name = " aditya gurav";
+let age = 25;
+ console.log(name);
+
+ console.log(` this is my name ${name} and my age is  ${age}`)
+
+ const gameName = new String ('this is my game')
+ console.log(gameName[13])
+ console.log(gameName.__proto__);
+ 
+ console.log(gameName.length);
+ 
+ 
