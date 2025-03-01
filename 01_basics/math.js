7e1d8aba314a82c104dc566b0850c242bf8b1c7b
Aditya-43 <69339559+Aditya-43@users.noreply.github.com>
2025-03-01 12:47:32 +0000

    Math

---
+++ 01_basics/math.js
@@ -0,0 +1,16 @@
+const num = 1000
+// console.log(num);
+
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(typeof(balance));
+
+
+//  MATHS 
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+
+
