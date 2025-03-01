009476e32aee3f43c9dbf62c2d555c5f5b45f52f
Aditya-43 <69339559+Aditya-43@users.noreply.github.com>
2025-03-01 13:39:15 +0000

    array

---
+++ 02_basics/1_Arrays.js
@@ -0,0 +1,14 @@
+// const myArray = [1,2,3,4,5]
+// console.log(myArray)
+// console.log(myArray[0]);
+
+// const superHero = new Array(1,3,4,5)
+// console.log(superHero);
+// console.log(superHero[2]);
+
+// superHero.push(6)
+// console.log(superHero);
+
+
+
+
